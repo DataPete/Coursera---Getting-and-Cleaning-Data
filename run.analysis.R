a29085c5654da312400dfbecf2824c62b5b44cc8
DataPete <PeterAnhNguyen@gmail.com>
2015-05-24 18:01:10 -0400

    Update run.analysis.R

--- run.analysis.R
+++ run.analysis.R
@@ -44,9 +44,9 @@ check_dataset <- function() {
     # Checks to see if UCI zip file is downloaded
     if (!file.exists("getdata_projectfiles_UCI HAR Dataset.zip")) {
         #Downloads UCI HAR Dataset
-        file_Url <- 
+        file.Url <- 
             "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-        download.file(file_Url, 
+        download.file(file.Url, 
                       dest = "getdata_projectfiles_UCI HAR Dataset.zip",
                       method = "internal")
         
@@ -74,63 +74,63 @@ analysis <- function() {
     setwd("./UCI HAR Dataset")
     
     # Read activity_labels and create id and activty headers
-    data_Act_Labels <- read.table("activity_labels.txt",
+    data.Act.Labels <- read.table("activity_labels.txt",
                                   col.names = c("label_id", "label_activity"))
     
     # Create headers from the features file
-    data_Features <- read.table("features.txt")
-    features_Headers <- tolower(data_Features[, 2])
+    data.Features <- read.table("features.txt")
+    features.Headers <- tolower(data.Features[, 2])
     
     # Read subject, x, and y tests from test folder
     setwd("./test")
-    data_Subject_Test <- read.table("subject_test.txt")
-    data_X_Test <- read.table("x_test.txt")
-    data_Y_Test <- read.table("y_test.txt")
+    data.Subject.Test <- read.table("subject_test.txt")
+    data.X.Test <- read.table("x_test.txt")
+    data.Y.Test <- read.table("y_test.txt")
     
     # Read subject, x, and y tests from train folder
     setwd("../train")
-    data_Subject_Train <- read.table("subject_train.txt")
-    data_X_Train <- read.table("x_train.txt")
-    data_Y_Train <- read.table("y_train.txt")
+    data.Subject.Train <- read.table("subject_train.txt")
+    data.X.Train <- read.table("x_train.txt")
+    data.Y.Train <- read.table("y_train.txt")
     
     # Label id and activity headers to subject test/train and Y test/train
-    colnames(data_Subject_Test) <- colnames(data_Subject_Train) <- "subject_id"
-    colnames(data_Y_Test) <- colnames(data_Y_Train) <- "activity_id"
+    colnames(data.Subject.Test) <- colnames(data.Subject.Train) <- "subject_id"
+    colnames(data.Y.Test) <- colnames(data.Y.Train) <- "activity_id"
     
     # Label features_Headers to X test/train
-    colnames(data_X_Test) <- colnames(data_X_Train) <- features_Headers
+    colnames(data.X.Test) <- colnames(data.X.Train) <- features.Headers
     
     # Merge Test files together
-    merge_Test <- cbind(data_Subject_Test, data_Y_Test, data_X_Test)
+    merge.Test <- cbind(data.Subject.Test, data.Y.Test, data.X.Test)
     
     # Merge Train files together
-    merge_Train <- cbind(data_Subject_Train, data_Y_Train, data_X_Train)
+    merge.Train <- cbind(data.Subject.Train, data.Y.Train, data.X.Train)
 
     # Merge both Test and Train merged files together
-    merge_All <- rbind(merge_Test, merge_Train)
+    merge.All <- rbind(merge.Test, merge.Train)
     
     # Extracts mean and standard deviation values from merged test/train file
-    extract_Mean <- grep("mean", names(merge_All))
-    extract_Std <- grep("std", names(merge_All))
-    extract_Mean_Names <- names(merge_All)[extract_Mean]
-    extract_Std_Names <- names(merge_All)[extract_Std]
+    extract.Mean <- grep("mean", names(merge.All))
+    extract.Std <- grep("std", names(merge.All))
+    extract.Mean.Names <- names(merge.All)[extract.Mean]
+    extract.Std.Names <- names(merge.All)[extract.Std]
     
     # Merges mean and standard deviation columns together
-    data_Mean_Std <- merge_All[, c("subject_id", "activity_id", extract_Mean_Names, extract_Std_Names)]
+    data.Mean.Std <- merge.All[, c("subject_id", "activity_id", extract.Mean.Names, extract.Std.Names)]
     
     # Merges Activity_labels dataset to the mean/std dataset
-    data_Act_MeanStd <- merge(data_Act_Labels, data_Mean_Std, by.x = "label_id",
+    data.Act.MeanStd <- merge(data.Act.Labels, data.Mean.Std, by.x = "label_id",
                         by.y = "activity_id", all = TRUE)
     
     # Retrieves the activity label, name of activity, subject, and the mean and std values
-    data_Melt <- melt(data_Act_MeanStd, id = c("label_id", "label_activity", "subject_id"))
+    data.Melt <- melt(data.Act.MeanStd, id = c("label_id", "label_activity", "subject_id"))
     
     # Casts melted data to find the mean
-    data_Mean <- dcast(data_Melt, label_id + label_activity + subject_id ~ variable, mean)
+    data.Mean <- dcast(data.Melt, label_id + label_activity + subject_id ~ variable, mean)
 
     # Sets directory back to UCI HAR Dataset folder and creates a copy of the tidy data
     setwd("..")
-    write.table(data_Mean, "./UCI HAR Tidy Data - Mean & Std")
+    write.table(data.Mean, "./UCI HAR Tidy Data - Mean & Std.txt")
     print("A copy of the mean and standard deviation extracted from the UCI HAR Dataset has been created")
-    print("The file name is UCI HAR Tidy Data - Mean & Std.  Thank you.")
+    print("The file name is UCI HAR Tidy Data - Mean & Std.txt  Thank you.")
 }
